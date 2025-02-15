8bb819756e2aa27ed11c35f5f01cc009310e99d0
rickoroman-qa <rroyahinf@gmail.com>
2025-02-15 07:20:17 +0700

    Update TestSuite1.cy.js

--- cypress/e2e/TestSuite1.cy.js
+++ cypress/e2e/TestSuite1.cy.js
@@ -1,24 +1,70 @@
-/// <reference types="cypress" />
-
 /* 
-  Welcome QA candidate! Good luck in writing your test script! 
-  You may structure or name your file however you see fit as this is just a template
+  rroyahinf@gmail.com
 */
 
-describe("Test Suite 1", () => {
+/**describe('99.co Home Page Tests', () => {
   beforeEach(() => {
-    //Optional
+    cy.visit('https://www.99.co/singapore', {
+      onBeforeLoad: (win) => {
+        Object.defineProperty(win.navigator, 'webdriver', {
+            get: () => undefined
+        });
+      },
+      failOnStatusCode: false,
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+        //'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Referer': 'https://www.google.com/'
+      }
+    });
   });
 
-  it("Test Case 1", () => {
-    //Write your automation script here for Test Case 1
+  it('should load the home page successfully', () => {
+    cy.url().should('include', 'https://www.99.co/singapore');
+    //cy.title().should('contain', 'Jual Beli Properti Rumah Apartemen | Rumah123');
+    cy.pause(); // 🔴 Cypress waits here until you resume manually
+    // After clicking the checkbox, continue execution
+    cy.contains('Continue').click();  
+  });
+});**/
+
+describe('99.co Home Page Tests', () => {
+  beforeEach(() => {
+    cy.visit('https://www.rumah123.com/', {
+      failOnStatusCode: false,
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Referer': 'https://www.google.com/'
+      }
+    });
   });
 
-  it("Test Case 2", () => {
-    //Optional
+  const textsToVerify = [
+    'Rekomendasi Sesuai Pencarianmu',
+    'Properti Pilihan Kami',
+    'Properti Terpopuler',
+    'Daftar Properti Baru',
+    'Kenapa Rumah123?',
+    'Kata Mereka yang Sudah Menggunakan Layanan Rumah123',
+    'Info Properti'
+  ];
+
+  it('should load the home page successfully', () => {
+    cy.url().should('include', 'https://www.rumah123.com/');
+    cy.title().should('contain', 'Jual Beli Properti Rumah Apartemen | Rumah123');
+    cy.wait(3000)
   });
 
-  it("Test Case 3", () => {
-    //Optional
+  it('close pop up', () =>{
+    //cy.get('.pdp-splash-screen__content__container__link > .ui-atomic-image').click(); // Change the selector based on your site
+    cy.get('body').click(0, 0); // Clicks top-left corner of the page
+  })
+  
+  it('should verify all required texts are visible', () => {
+    textsToVerify.forEach((text) => {
+      cy.contains(text, { timeout: 10000 }).scrollIntoView().should('be.visible');
+    });
   });
 });
